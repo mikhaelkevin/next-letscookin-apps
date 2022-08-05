5727334cd307e3e8d7a99a10d488050a189d502e
Mikhael <goodboybutnot@gmail.com>
2022-08-06 03:20:44 +0700

    add delete recipe feature

---
+++ pages/api/deleteRecipe.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const deleteRecipe = async (req, res) => {
+  try {
+    const { recipeId, token } = req.query;
+    const response = await axios.delete(
+      `${process.env.CLIENT_API_URL}/letscookinapps/recipes`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: { id: recipeId },
+      }
+    );
+    res.status(200).send(response?.data);
+  } catch (error) {
+    console.log("error", error);
+    const status = error?.response?.status || 500;
+    const message = error?.response?.data?.message;
+    res.status(status).send(message);
+  }
+};
+
+export default deleteRecipe;
--- pages/profile/my-recipe.js
+++ pages/profile/my-recipe.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../../styles/MyRecipe.module.css";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
@@ -8,8 +9,13 @@ import axios from "axios";
 
 const MyRecipePage = () => {
   const { user, token } = useSelector((state) => state?.auth);
-  const [myRecipe, setMyRecipe] = useState([]);
   const router = useRouter();
+
+  const [myRecipe, setMyRecipe] = useState([]);
+  const [appendDelAlert, setAppendDelAlert] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [delTarget, setDelTarget] = useState(0);
+  const [isSuccess, setIsSuccess] = useState(false);
 
   const getMyRecipe = async (userId) => {
     try {
@@ -20,15 +26,38 @@ const MyRecipePage = () => {
     }
   };
 
+  const requestDelRecipe = async (recipeId) => {
+    try {
+      setIsLoading(true);
+      await axios("/api/deleteRecipe", {
+        params: {
+          recipeId,
+          token,
+        },
+      });
+      setTimeout(() => {
+        setIsSuccess(true);
+      }, 3000);
+    } catch (error) {
+      console.log("error :>> ", error.response);
+    }
+  };
+
   useEffect(() => {
+    setIsSuccess(false);
+    setAppendDelAlert(false);
+    setIsLoading(false);
+    setDelTarget(0);
     getMyRecipe(user?.id);
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSuccess]);
 
   return (
     <>
       <Head>
         <title>My Recipe</title>
       </Head>
+
       <div className={styles.mainPage}>
         <div className={styles.header}>
           <button className="btn btn-light">
@@ -43,28 +72,71 @@ const MyRecipePage = () => {
           <h2>My Recipe</h2>
         </div>
         <div className={styles.content}>
+          {appendDelAlert && (
+            <div className="alert alert-danger text-center ">
+              <h2>Are you sure want to delete the recipe?</h2>
+              <div className="my-3">
+                <button
+                  className="btn btn-primary m-2"
+                  onClick={() => requestDelRecipe(delTarget)}
+                  disabled={isLoading}
+                >
+                  {isLoading ? "Deleting..." : "Yes, delete it"}
+                </button>
+                <button
+                  className="btn btn-danger m-2"
+                  onClick={() => setAppendDelAlert(false)}
+                >{`No, it's a mistake`}</button>
+              </div>
+            </div>
+          )}
           {myRecipe?.map((recipe) => (
             <div className="card box-shadow" key={recipe.id}>
               <div className="row d-flex justify-content-center align-items-center">
                 <div className="col-3 ">
-                  <Image
-                    src={
-                      recipe?.recipe_picture
-                        ? recipe?.recipe_picture
-                        : "/images/image_notfound.png"
-                    }
-                    width="100%"
-                    height="100%"
-                    alt="card-image"
-                  />
+                  <Link href={`/recipe/${recipe.id}`}>
+                    <a>
+                      <Image
+                        src={
+                          recipe?.recipe_picture
+                            ? recipe?.recipe_picture
+                            : "/images/image_notfound.png"
+                        }
+                        width="100%"
+                        height="100%"
+                        alt="card-image"
+                      />
+                    </a>
+                  </Link>
                 </div>
-                <div className="col-6">
+                <div className="col-7">
                   <h3>{recipe.title}</h3>
                 </div>
-                <div className="col-3">
-                  <span>Edit</span>
-                  <span>Delete</span>
+                <div className="col-2">
+                  <div className={styles.recipeMenu}>
+                    <span>
+                      <Image
+                        src="/images/edit-icon.svg"
+                        width="30px"
+                        height="30px"
+                        alt="edit-icon"
+                      />
+                    </span>
+                    <span>
+                      <Image
+                        src="/images/delete-icon.svg"
+                        width="30px"
+                        height="30px"
+                        alt="delete-icon"
+                        onClick={() => {
+                          setAppendDelAlert(true);
+                          setDelTarget(recipe?.id);
+                        }}
+                      />
+                    </span>
+                  </div>
                 </div>
+                <hr />
               </div>
             </div>
           ))}
