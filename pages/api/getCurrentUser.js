ca4446172a542d754f6d6d7f8018346b25fb97c1
Mikhael <goodboybutnot@gmail.com>
2022-09-09 15:28:42 +0700

    fix(url): api url error fix.

--- pages/api/getCurrentUser.js
+++ pages/api/getCurrentUser.js
@@ -4,14 +4,15 @@ const getCurrentUser = async (req, res) => {
   try {
     const { userId } = req.query;
     const response = await axios.get(
-      `${process.env.CLIENT_API_URL}letscookinapps/users/${userId}`
+      `${process.env.CLIENT_API_URL}/letscookinapps/users/${userId}`
     );
     res.status(200).send(response?.data);
   } catch (error) {
+    console.log(error)
     const status = error?.response?.status
     const message = error?.response?.data || error?.response?.data?.message
     res.status(status).send(message)
-}
+  }
 };
 
 export default getCurrentUser;
