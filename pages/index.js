0025f033506e6429be8d4ca2ed1572cbddf809a7
Mikhael <goodboybutnot@gmail.com>
2022-09-09 08:52:09 +0700

    change url format on index

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,11 @@ import React from "react";
 export const getStaticProps = async (req, res) => {
   try {
     const newRecipeResponse = await fetch(
-      `${process.env.API_URL}letscookinapps/recipes/new-recipe/`
+      `${process.env.API_URL}/letscookinapps/recipes/new-recipe/`
     );
 
     const recipeList = await fetch(
-      `${process.env.API_URL}letscookinapps/recipes/`
+      `${process.env.API_URL}/letscookinapps/recipes/`
     );
 
     const recipeListData = await recipeList?.json();
